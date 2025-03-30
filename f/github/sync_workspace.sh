a7ad2bc7989d63223ae474d7f8ee8505c3ddd34a
rconjoe <root@trog.codes>
2025-03-29 19:59:57 -0700

    init

---
+++ f/github/sync_workspace.sh
@@ -0,0 +1,27 @@
+# shellcheck shell=bash
+set -e
+
+token=$(curl -s -H "Authorization: Bearer $WM_TOKEN" \
+  "$BASE_INTERNAL_URL/api/w/$WM_WORKSPACE/variables/get_value/f/github/access_token" | jq -r .)
+
+npm i -g windmill-cli
+
+git clone https://rconjoe:$token@github.com/rconjoe/proxmill.git
+cd mill
+
+git config user.name "rconjoe"
+git config user.email "root@trog.codes"
+
+wmill workspace add playground playground https://proxmill.trog.codes --token "$WM_TOKEN"
+
+wmill sync pull --yes
+
+date=$(date "+%Y-%m-%d %H:%M:%S")
+
+git add .
+git commit -m "sync $date"
+
+# Push changes
+git push https://rconjoe:$token@github.com/rconjoe/proxmill.git master
+
+echo "Script finished."
